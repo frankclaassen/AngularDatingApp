4a4dc87cf9e9eb0f6317ebeae1fd1e0f96eb714b
Frank Claassen <frank@datajunxion.com>
2023-12-08 14:16:50 +0800

    section 12 - user registration

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using API.Data;
 using API.DTO;
 using API.Entities;
 using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -14,11 +15,13 @@ public class AccountController : BaseApiController
 {
   private readonly DataContext _context;
   private readonly ITokenService _tokenService;
+  private readonly IMapper _mapper;
 
-  public AccountController(DataContext context, ITokenService tokenService)
+  public AccountController(DataContext context, ITokenService tokenService, IMapper mapper)
   {
     _context = context;
     _tokenService = tokenService;
+    _mapper = mapper;
   }
 
   [AllowAnonymous]
@@ -30,14 +33,13 @@ public class AccountController : BaseApiController
       return BadRequest("User already exists");
     }
 
+    var user = _mapper.Map<AppUser>(registerDto);
+
     using var hmac = new HMACSHA512();
 
-    var user = new AppUser
-    {
-      UserName = registerDto.Username.ToLower(),
-      PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-      PasswordSalt = hmac.Key
-    };
+    user.UserName = registerDto.Username.ToLower();
+    user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
+    user.PasswordSalt = hmac.Key;
 
     _context.Users.Add(user);
     await _context.SaveChangesAsync();
@@ -45,6 +47,7 @@ public class AccountController : BaseApiController
     return Ok(new UserDto
     {
       Username = user.UserName,
+      KnownAs = user.KnownAs,
       Token = _tokenService.CreateToken(user)
     });
   }
@@ -68,6 +71,7 @@ public class AccountController : BaseApiController
     return Ok(new UserDto
     {
       Username = user.UserName,
+      KnownAs = user.KnownAs,
       Token = _tokenService.CreateToken(user),
       PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
     });
